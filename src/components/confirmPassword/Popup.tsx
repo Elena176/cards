d357dd72dec3540eb027c6e40a217df61c39ed56
Elena176 <lena.tdn176@gmail.com>
2022-07-19 01:33:20 +0300

    refactor styles for signUp, confirmPassword

--- src/components/confirmPassword/Popup.tsx
+++ src/components/confirmPassword/Popup.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useNavigate } from 'react-router-dom';
 
 import letter from '../../assets/letter.png';
+import { PATH } from '../../enum';
 import style from '../../style/Common.module.css';
 import stylePopup from '../../style/Popup.module.css';
 
 import { ReturnComponentType } from 'types';
 
-export const Popup = (): ReturnComponentType => (
-  <div className={style.mainContainer}>
-    <div className={style.content}>
-      <img className={stylePopup.letterImg} src={letter} alt="letter" />
-      <div className={style.contentWrap}>
-        <h2 className={stylePopup.heading}>Check Email</h2>
-        <p>We have sent the Email with instructions to your email </p>
+const time = 3000;
+export const Popup = (): ReturnComponentType => {
+  const navigate = useNavigate();
+  function handleSubmit(): void {
+    navigate(PATH.CREATE_NEW_PASSWORD);
+  }
+  useEffect(() => {
+    setTimeout(() => handleSubmit, time);
+  }, []);
+  return (
+    <div className={style.mainContainer}>
+      <div className={`${style.content} + ${stylePopup.content}`}>
+        <img className={stylePopup.letterImg} src={letter} alt="letter" />
+        <div className={stylePopup.contentWrap}>
+          <h2 className={stylePopup.heading}>Check Email</h2>
+          <p>We have sent an Email with instructions to your email </p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
